0d0bad2f49bf65b71ce273fca35ecc9f16ab4c1a
Ekundayo <ekundayomustapha92@gmail.com>
2025-08-11 00:58:42 +0100

    correcting css doughnut chart

--- src/pages/doughnutChart/DoughNut.jsx
+++ src/pages/doughnutChart/DoughNut.jsx
@@ -1,4 +1,4 @@
-import styles from "../doughnutChart/doughnut.module.css";
+import styles from "../doughnutChart/doughNut.module.css";
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
