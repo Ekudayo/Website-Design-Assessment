2b82ef7fa45865b2bfc7b96b6c26936da644b847
Ekundayo <ekundayomustapha92@gmail.com>
2025-08-11 22:14:55 +0100

    fixing Arrow image error

--- src/pages/our/Our.jsx
+++ src/pages/our/Our.jsx
@@ -23,7 +23,7 @@ const Our = () => {
             <h3>A day with our wonderful children </h3>
           </div>
           <div className={styles.arrow}>
-            <img src="./src/assets/Arrow.png" alt="Arrow" />
+            <img src={Arrow} alt="Arrow" />
           </div>
         </div>
         <div className={styles.down}>
